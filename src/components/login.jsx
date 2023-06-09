be695856305a3ea03cc5a5b463a1547363dbcae7
Vinothjv10 <vinothjv10@gmail.com>
2023-06-09 17:10:45 +0530

    initial commit

---
+++ src/components/login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <section>
+        <div className='login_page'>
+            <div className='btn'>
+                <button>Google</button>
+            </div>
+            <div className='btn'>
+                <button>FaceBook</button>
+            </div>
+            <div className='btn'>
+                <button>Github</button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default login
