c96df8ddb1ec10aa092d1a08bf482296cf08c4a6
Vinothjv10 <vinothjv10@gmail.com>
2023-06-10 23:22:05 +0530

    overall sigin inputs set

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React from 'react';
-import '../src/App.css';
-
-import {auth,provider} from "./firebase"
-import {signInWithPopup} from "firebase/auth"
-import { useState } from 'react'
-
+import React from 'react'
+import Signin from './components/Signin'
 
 const App = () => {
-
-  const [value, setUser] = useState(null);
-
-  const googleLogin = () =>{
-        signInWithPopup(auth,provider).then((data)=>{
-            setUser(data.user.email)
-            localStorage.setItem("email",data.user.email)
-        })
-  }
-
   return (
-    
-      <section>
-         <div className='login_page'>
-
-
-             <div className='btn' >
-                <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" alt="" />
-                 <button onClick={googleLogin}>Google</button>
-             </div>
-
-
-             <div className='btn'>
-                 <img src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png" alt="" />
-                 <button>FaceBook</button>
-             </div>
-
-
-            <div className='btn'>
-                 <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" />
-                 <button>Github</button>
-             </div>
-         </div>
-        {value}
-    </section>
+    <Signin></Signin>
   )
 }
 
